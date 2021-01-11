516feb6aebe093b7088455f4cea38069f7805f36
LksDesings <76118291+LksDesings@users.noreply.github.com>
2021-01-11 15:59:37 -0300

    crear servlets

--- src/controlador/CrearAdministrativoServlet.java
+++ src/controlador/CrearAdministrativoServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import implementacion.UsuarioImpl;
+import modelo.Cliente;
+
 /**
  * Servlet implementation class CrearAdministrativoServlet
  */
@@ -36,6 +39,30 @@ public class CrearAdministrativoServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		int rut = Integer.parseInt(request.getParameter("run"));
+		String nombre = request.getParameter("nombre");
+		String apellidos = request.getParameter("apellido");
+		String email = request.getParameter("email");
+		String area = request.getParameter("area");
+		int usuariorun = Integer.parseInt(request.getParameter("runusuario"));
+
+		
+		Administrativo adm = new Administrativo(rut,nombre,apellidos,email,area,usuariorun);
+		UsuarioImpl usrimpl = new UsuarioImpl();
+		boolean res = usrimpl.crearAdministrativo(adm);
+		
+		String msg = "";
+		
+		if (res) {
+			msg = "El Administrativo se agregó correctamente";
+		}
+		else {
+			msg = "No se pudo agregar al Administrativo";
+		}
+		
+		request.setAttribute("mensaje", msg);
+	
+		request.getRequestDispatcher("ConfirmaCrearUsuario.jsp").forward(request, response);
 	}
 
 }
