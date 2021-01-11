125467788f8322793650562de51e52911f9d520e
LksDesings <76118291+LksDesings@users.noreply.github.com>
2021-01-11 15:58:52 -0300

    nuevos servlets

---
+++ src/controlador/CrearProfesionalServlet.java
@@ -0,0 +1,70 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import implementacion.UsuarioImpl;
+import modelo.Cliente;
+
+/**
+ * Servlet implementation class CrearProfesionalServlet
+ */
+@WebServlet("/CrearProfesionalServlet")
+public class CrearProfesionalServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CrearProfesionalServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		int rut = Integer.parseInt(request.getParameter("run"));
+		String nombre = request.getParameter("nombre");
+		String apellidos = request.getParameter("apellido");
+		String telefono = request.getParameter("telefono");
+		String titulo = request.getParameter("titulo");
+		String proyecto = request.getParameter("proyecto");
+		int usuariorun = Integer.parseInt(request.getParameter("runusuario"));
+	
+
+		
+		Profesional pro = new Profesional(rut,nombre,apellidos,telefono,titulo,proyecto,usuariorun);
+		UsuarioImpl usrimpl = new UsuarioImpl();
+		boolean res = usrimpl.crearProfesional(pro);
+		
+		String msg = "";
+		
+		if (res) {
+			msg = "El Profesional se agregó correctamente";
+		}
+		else {
+			msg = "No se pudo agregar al Profesional";
+		}
+		
+		request.setAttribute("mensaje", msg);
+	
+		request.getRequestDispatcher("ConfirmaCrearUsuario.jsp").forward(request, response);
+	}
+
+}
